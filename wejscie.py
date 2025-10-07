0f6e1fca28a2b216eb6e9e930721804d8b2309cd
aliaksei tsialpuk <ALITSIA80@edu.gdansk.pl>
2025-10-07 14:34:02 +0200

    lekcja nqa temat wejscia

---
+++ wejscie.py
@@ -0,0 +1,16 @@
+#1) wprowadzanie danych przez urzytkownika do programu
+
+#1.utworzenie zmiennej
+#2.na ekranie pojawia się komunikat z inputa
+#3) program się zatrzyoje i czeka na podanie danych i ENTER
+#4) po ENTER to co wpiszemy napis trafi do zmiennej jako string
+#5) i program wykonuje się dalej
+
+imie = input('podaj swoje imie')
+
+liczba = int(input('podaj licznę'))
+print(type(liczba))
+
+liczba_z_przecinkiem = float(input('podaj liczbę z przecinkiem'))
+print(type(liczba_z_przecinkiem))
+
