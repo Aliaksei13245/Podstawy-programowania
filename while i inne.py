e7d200f4ced7ec7eed8bb2523d32929f5508c835
aliaksei tsialpuk <ALITSIA80@edu.gdansk.pl>
2025-12-09 14:30:59 +0100

    while i inne

---
+++ while i inne.py
@@ -0,0 +1,17 @@
+'''liczba = 1898864
+licznik = 0
+while liczba > 0:
+    liczba = liczba // 2
+    licznik = licznik + 1
+    print(liczba)
+print(licznik)'''
+
+
+#zad1
+'''x = input('podaj liczbę lub q buy zakończyć')
+licznik = 0
+while x != 'q':
+    liczba = int(x)
+    if liczba < 2:
+        licznik = licznik + 1
+    x = input('podaj liczbę lub q aby zakończć')'''
