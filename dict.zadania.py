2daaea56f8d54fc825612fcd71a7532bc9a3b71a
aliaksei tsialpuk <ALITSIA80@edu.gdansk.pl>
2026-02-16 11:20:42 +0100

    dicty2

--- dict.zadania.py
+++ dict.zadania.py
@@ -1,4 +1,4 @@
-zbior = set()
+'''zbior = set()
 lista2d = [
 [5, 2, 8, 5, 1],
 [3, 8, 2, 9, 5],
@@ -26,4 +26,43 @@ for x in range(len(lista2d)):
         lista1d.append(element)
 lista_zbior = set(lista1d)
 for e in lista_zbior:
-    print(e, lista1d.count(e))
+    print(e, lista1d.count(e))'''
+
+
+slowa = [
+"LETTER",
+"BALLOON",
+"SUCCESS",
+"HAPPY",
+"COFFEE",
+"BOOKKEEPER",
+"ASSESS",
+"MISSISSIPPI",
+"ADDRESS",
+"TOOLBOX"
+]
+
+#slowo = 'lETTER'
+#slowo_zbior = set(slowo)
+max_lrl = 0
+for x in slowa:
+    x_zbior = set(x)
+    lrl = len(x_zbior)
+    if lrl > max_lrl:
+        max_lrl = lrl
+        max_x = x
+
+
+#zadanie 2.2
+zbior = set()
+for x in slowa:
+    for y in x:
+        zbior.add(y)
+
+for l in zbior:
+    lista = []
+    for s in slowa:
+        if l in s:
+            lista.append(s)
+    print(f'{l}:{lista}')
+
